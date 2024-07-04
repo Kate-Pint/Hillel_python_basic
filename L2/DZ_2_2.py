019df4553ab22335915ccc5a85f62d267263d33a
Kateryna_Pint <pint.katerinka@gmail.com>
2024-07-04 14:55:01 +0300

    DZ_2_2 commit

--- L2/DZ_2_2.py
+++ L2/DZ_2_2.py
@@ -4,4 +4,5 @@ b = (cust_number % 100) // 10
 c = (cust_number // 100) % 10
 d = (cust_number // 1000) % 10
 e = cust_number // 10000
-print(str(a)+str(b)+str(c)+str(d)+str(e))
+reversed_number = a * 10000 + b * 1000 + c * 100 + d * 10 + e
+print(reversed_number)
