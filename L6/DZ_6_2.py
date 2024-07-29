a30ea67a65cba1d9ffdb5a862a98f47596da5d54
Kateryna_Pint <pint.katerinka@gmail.com>
2024-07-29 18:53:01 +0300

    DZ_6_2 commit

---
+++ L6/DZ_6_2.py
@@ -0,0 +1,11 @@
+number = int(input("Type a number: "))
+
+while number > 9:
+    i = 1
+    while number > 0:
+        digit = number % 10
+        i *= digit
+        number //= 10
+    number = i
+
+print(number)
